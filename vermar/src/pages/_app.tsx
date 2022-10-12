a0fab4e2ba9c15bdbb2503c2138b31b6e34fdb0e
Al Joseph Condino <condinoaljoseph@gmail.com>
2022-10-12 22:17:36 +0800

    include credentials

--- vermar/src/pages/_app.tsx
+++ vermar/src/pages/_app.tsx
@@ -6,6 +6,7 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
+  credentials: "include",
   cache: new InMemoryCache(),
 });
 
