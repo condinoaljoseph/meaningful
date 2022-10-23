34c979c478ee92df979b8938f31e8010df921975
Al Joseph Condino <condinoaljoseph@gmail.com>
2022-10-23 21:47:25 +0800

    setup next apollo for ssr cookie forwarding

--- vermar/src/pages/index.tsx
+++ vermar/src/pages/index.tsx
@@ -1,24 +1,27 @@
 import { ExclamationCircleIcon } from "@heroicons/react/24/solid";
-import type { NextPage } from "next";
+import { Layout } from "../components/Layout";
 import { PostsList } from "../components/PostsList";
 import { Block } from "../components/ui/Block";
+import withApollo from "../utils/withApollo";
 
-const Home: NextPage = () => {
+const Home = () => {
   return (
-    <div className="lg:flex">
-      <div className="relative w-full pr-0 lg:w-3/4 lg:pr-5">
-        <PostsList />
-      </div>
+    <Layout>
+      <div className="lg:flex">
+        <div className="relative w-full pr-0 lg:w-3/4 lg:pr-5">
+          <PostsList />
+        </div>
 
-      <div className="w-full lg:w-4/12 lg:min-w-[321px]">
-        <Block className="lg:fixed lg:w-[320px] mt-4 lg:mt-0">
-          <ExclamationCircleIcon className="w-[1em] h-[1em] inline mr-1" />
-          meaningful is still in the beta phase, things may break, please handle
-          us with care.
-        </Block>
+        <div className="w-full lg:w-4/12 lg:min-w-[321px]">
+          <Block className="lg:fixed lg:w-[320px] mt-4 lg:mt-0">
+            <ExclamationCircleIcon className="w-[1em] h-[1em] inline mr-1" />
+            meaningful is still in the beta phase, things may break, please
+            handle us with care.
+          </Block>
+        </div>
       </div>
-    </div>
+    </Layout>
   );
 };
 
-export default Home;
+export default withApollo({ ssr: true })(Home);
